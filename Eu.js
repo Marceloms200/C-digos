071b7347cefb018149c46ade1e457b5ca7eb7546
marcelo <celoms2002@gmail.com>
2024-12-28 23:32:06 -0300

    commit2

---
+++ Eu.js
@@ -0,0 +1,5 @@
+let nome = "Marcelo"; // string
+let idade = 22;    // number
+let isAtivo = true; // boolean
+let frutas = ["pera", "ameixa", "goiaba"]; // array
+let pessoa = { nome: "Marcelo", idade: 22 };  // objeto
